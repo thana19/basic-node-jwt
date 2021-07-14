ae559edae5d426285f8b9d46c271c43ce269e405
thana19 <thana.j@gmail.com>
2021-07-14 11:49:04 +0700

    dev2

---
+++ index.js
@@ -0,0 +1,18 @@
+const buildApp = require('./src/app')
+const config = require('./src/config')
+
+const startApp = async () => {
+    const appOptions = {
+        logger: true
+    }
+    const app = buildApp(applications)
+
+    try{
+        app.listen(config.port, config.hostname)
+        console .log(`app is listening on port ${config.port}`)
+    } catch (error){
+      throw error
+    }
+}
+
+startApp()

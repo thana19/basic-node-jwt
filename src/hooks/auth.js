e36681d29addbf533b0b8e491e910c5467198396
thana19 <thana.j@gmail.com>
2021-07-16 23:52:43 +0700

    fix statusCode 500 to 401

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -2,15 +2,27 @@ const jwt = require ('jsonwebtoken')
 const config = require('../config')
 
 const validateToken = async (request, reply) => {
-    const { authorization } = request.headers
+    try {
+        const { authorization } = request.headers
 
-    if (!authorization) {
-        throw new Error('missing authorization in headers')
+        if (!authorization) {
+            throw new Error('missing authorization in headers')
+        }
+    
+        const token = authorization.split(' ')[1]
+    
+        console.log('token->', jwt.decode(token))
+        // token-> { 
+        //     id: '60f14345ec6685248ed03d81', 
+        //     iat: 1626453657, 
+        //     exp: 1626453777 
+        // }
+    
+        await jwt.verify(token, config.secretKey)
+    } catch (error) {
+        reply.statusCode = 401
+        throw error
     }
-
-    const token = authorization.split(' ')[1]
-
-    await jwt.verify(token, config.secretKey)
 }
 
 module.exports = {
